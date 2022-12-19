77c34310270e7c656c06bdc3f93ac64a33d5efa2
nicolegromek <111621297+nicolegromek@users.noreply.github.com>
2022-12-19 02:08:13 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,19 +50,15 @@ jQuery(document).ready(function($) {
         $('.owl-carousel').owlCarousel({
             loop:true,
             margin:30,
-            autoWidth:25,
+            items:5,
             responsiveClass:true,
             responsive:{
                 0:{
                     items:1,
                     nav:true
                 },
-                600:{
-                    items:2,
-                    nav:false
-                },
                 1000:{
-                    items:3,
+                    items:2,
                     nav:true,
                     loop:false
                 }
